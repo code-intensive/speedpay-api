f7254bcd44eeb1427b6a1af404a0c9caf303e289
Tega Ukavwe <justtega97@gmail.com>
2022-12-13 14:55:17 +0100

    set up speedpay auth app

---
+++ speedpay_auth/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from rest_framework_simplejwt import views as simple_jwt_views
+
+app_name = "auth"
+
+urlpatterns = [
+    path("login/", simple_jwt_views.token_obtain_pair, name="auth-login"),
+    path("logout/", simple_jwt_views.token_blacklist_view, name="auth-logout"),
+    path("refresh-token/", simple_jwt_views.token_refresh, name="auth-token-refresh"),
+]
