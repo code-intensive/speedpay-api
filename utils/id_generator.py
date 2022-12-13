b50da357124dbdac89da4c6e698bd8784eb1f2b3
Tega Ukavwe <justtega97@gmail.com>
2022-12-13 15:00:45 +0100

    created wallet application

---
+++ utils/id_generator.py
@@ -0,0 +1,18 @@
+from uuid import uuid4
+
+
+def generate_uuid(prefix: str) -> str:
+    """Generates a uuid with a prefix.
+
+    :param prefix: string to be prepended to the
+    generated uuid's hexadecimal representation.
+
+    :return: UUID's hexadecimal representation with prefix.
+
+    :rtype: str.
+    """
+
+    def create_uuid(prefix):
+        return f"{ prefix }_{ uuid4().hex }"
+
+    return create_uuid(prefix)
